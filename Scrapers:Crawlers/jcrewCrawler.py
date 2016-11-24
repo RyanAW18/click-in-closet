0f17ddd222698994286773c968e4a3fb73e2c3f1
RAWhitf18 <ryanwhitfield27@gmail.com>
2016-11-23 23:06:28 -0500

    Almost complete oldnavyCrawler

--- Scrapers:Crawlers/jcrewCrawler.py
+++ Scrapers:Crawlers/jcrewCrawler.py
@@ -12,12 +12,16 @@ products.create_index([("name", "text"), ("brand", "text")])
 
 base_url = "http://www.jcrew.com"
 
+urlTest = "https://www.jcrew.com/p/mens_category/shirts/secretwash/secret-wash-shirt-in-redandgreen-tartan-heather-poplin/F8040"
+html = requests.get(urlTest)
+soup = BeautifulSoup(html.text, "html.parser")
 
 def findBrand(soup):
   return "JCrew"
 
 def findPrice(soup):
-  price = soup.find('span', class_='product__variation--price-list product__price--list')
+  price = soup.find('span', class_='tile__detail tile__detail--price--list')
+  print price
   if price is not None:
     return price.text
 
@@ -28,65 +32,70 @@ def findProductName(soup):
 
 def findImageLink(soup):
   image = soup.find('div', class_='product__image')
-  links = image.findAll('src')
-  for link in links:
-    if link is not None:
-      return link
+  return image
+  # links = image.findAll('src')
+  # for link in links:
+  #   if link is not None:
+  #     return link
 
 def findDescription(soup):
   description = soup.find('p', {"class" : "intro"})
   if description is not None:
     return description.text
 
-def postProduct(url):
-	html = urllib.urlopen(url).read()
-	soup = BeautifulSoup(html, "html.parser")
-	product = {'brand': findBrand(soup), 'name': findProductName(soup), 'price': findPrice(soup), 'image': findImageLink(soup), 'description': findDescription(soup), 'url': url}
-	products.insert(product)
+# def postProduct(url):
+# 	html = urllib.urlopen(url).read()
+# 	soup = BeautifulSoup(html, "html.parser")
+# 	product = {'brand': findBrand(soup), 'name': findProductName(soup), 'price': findPrice(soup), 'image': findImageLink(soup), 'description': findDescription(soup), 'url': url}
+# 	products.insert(product)
+
+print findBrand(soup)
+print findPrice(soup)
+print findProductName(soup)
+print findImageLink(soup)
+print findDescription(soup)
+
+# totalLinks = []
+# j = 0
+
+# # bigMenu = ["http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_W&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5585&mlink=5151,11140339,Top_nav_WP&visnav=1&clink=11140339",
+# # "http://www.oldnavy.com/browse/division.do?cid=5758&mlink=5151,11140339,Top_nav_Mat&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_M&visnav=1&clink=11140339", 
+# # "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_G&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_B&visnav=1&clink=11140339"]
+
+# html = requests.get(base_url)
+# soup = BeautifulSoup(html.text, "html.parser")
+# categories = soup.findAll('div', {"class" : "menu__item"})
+# for link in categories:
+#   allLinks = link.findAll("a")
+#   for a in allLinks:
+#     totalLinks.append(a["href"])
 
 
+# for link in totalLinks:
+#   new_url = base_url + link
+#   print new_url
+#   html = requests.get(new_url)
+#   soup = BeautifulSoup(html.text, "html.parser")
+#   while len(soup.select("div.c-product-tile a")) != 0:
+#     for a in soup.select("div.c-product-tile a"):
+#       j = j + 1
+#       productUrl = base_url + a["href"]
+#       print productUrl
+#       # postProduct(productUrl)
+#     pageHtml = soup.find("div", class_= "category__pagination")
+#     for div in pageHtml:
+#       links = div.findAll('a')
+#       nullLink = div.find('span', class_='pagination__link is-disabled')
+#       for link in links:
+#         if nullLink is not None:
+#           break
+#         pageUrl = link['href']
+#     if nullLink is not None:
+#       break
+#     #need to fix the product urls. it will not work without it. go to pagination href for clarification
+#     pageUrl = pageUrl.replace("..", "")
+#     pageUrl = base_url + pageUrl
+#     print j
 
-totalLinks = []
-j = 0
-
-# bigMenu = ["http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_W&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5585&mlink=5151,11140339,Top_nav_WP&visnav=1&clink=11140339",
-# "http://www.oldnavy.com/browse/division.do?cid=5758&mlink=5151,11140339,Top_nav_Mat&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_M&visnav=1&clink=11140339", 
-# "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_G&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_B&visnav=1&clink=11140339"]
-
-html = requests.get(base_url)
-soup = BeautifulSoup(html.text, "html.parser")
-categories = soup.findAll('div', {"class" : "menu__item"})
-for link in categories:
-  allLinks = link.findAll("a")
-  for a in allLinks:
-    totalLinks.append(a["href"])
-
-
-for link in totalLinks:
-  new_url = base_url + link
-  print new_url
-  html = requests.get(new_url)
-  soup = BeautifulSoup(html.text, "html.parser")
-  while len(soup.select("div.c-product-tile a")) != 0:
-    for a in soup.select("div.c-product-tile a"):
-      j = j + 1
-      productUrl = base_url + a["href"]
-      print productUrl
-      # postProduct(productUrl)
-    pageHtml = soup.find("div", class_= "category__pagination")
-    for div in pageHtml:
-      links = div.findAll('a')
-      nullLink = div.find('span', class_='pagination__link is-disabled')
-      for link in links:
-        if nullLink is not None:
-          break
-        pageUrl = link['href']
-    if nullLink is not None:
-      break
-    #need to fix the product urls. it will not work without it. go to pagination href for clarification
-    pageUrl = pageUrl.replace("..", "")
-    pageUrl = base_url + pageUrl
-    print j
-
-print j
-print "done"
+# print j
+# print "done"
--- Scrapers:Crawlers/oldnavyCrawler.py
+++ Scrapers:Crawlers/oldnavyCrawler.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import html5lib
+import platform
 import urllib
 import urllib2
 import requests
@@ -12,68 +15,114 @@ products.create_index([("name", "text"), ("brand", "text")])
 
 base_url = "http://www.oldnavy.com"
 
+# driver = webdriver.PhantomJS()
+
+# driver.get("http://oldnavy.gap.com/browse/product.do?cid=84470&vid=1&pid=275038022")
+
+# html = driver.page_source
+
+# driver.close()
+
+# soup = BeautifulSoup(html, "html.parser")
 
 def findBrand(soup):
   return "Old Navy"
 
 def findPrice(soup):
-  price = soup.find('h5', class_='product-price')
+  price = soup.find('span', class_='product-price--highlight')
   if price is not None:
-    return price.text
+    priceTag = price.text.strip()
+    return priceTag
 
 def findProductName(soup):
   product = soup.find('h1', class_='product-title')
   if product is not None:
-    return product.text
+    productName = product.text.strip()
+    return productName
 
+# not giving src link for the image
 def findImageLink(soup):
-  image = soup.find('div', class_='product-photo')
-  links = image.findAll('a')
-  for link in links:
-    if link["href"] is not None:
-      otherUrl = base_url + link["href"]
-      return otherUrl
+  image = soup.find('img', class_="product-photo--image")
+  links = image['src']
+  return links
 
-# def findDescription(soup):
-#   description = soup.find('span', {"itemprop" : "description"})
-#   if description is not None:
-#     return description.text
+def findDescription(soup):
+  description = soup.findAll("ul", class_="sp_top_sm dash-list")[1]
+  if description is not None:
+    return description.text.strip()
+  else:
+    return "No description available for this item"
 
-def postProduct(url):
-	html = urllib.urlopen(url).read()
-	soup = BeautifulSoup(html, "html.parser")
-	product = {'brand': findBrand(soup), 'name': findProductName(soup), 'price': findPrice(soup), 'image': findImageLink(soup), 'description': findDescription(soup), 'url': url}
-	products.insert(product)
+# def postProduct(url):
+# 	html = urllib.urlopen(url).read()
+# 	soup = BeautifulSoup(html, "html.parser")
+# 	product = {'brand': findBrand(soup), 'name': findProductName(soup), 'price': findPrice(soup), 'image': findImageLink(soup), 'description': findDescription(soup), 'url': url}
+# 	products.insert(product)
 
+# print findBrand(soup)
+# print findPrice(soup)
+# print findProductName(soup)
+# print findImageLink(soup)
+# print findDescription(soup)
 
 
 totalLinks = []
+linkTotal = 0
 j = 0
+nextText = ""
 
 bigMenu = ["http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_W&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5585&mlink=5151,11140339,Top_nav_WP&visnav=1&clink=11140339",
 "http://www.oldnavy.com/browse/division.do?cid=5758&mlink=5151,11140339,Top_nav_Mat&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_M&visnav=1&clink=11140339", 
 "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_G&visnav=1&clink=11140339", "http://www.oldnavy.com/browse/division.do?cid=5360&mlink=5151,11140339,Top_nav_B&visnav=1&clink=11140339"]
 
 for menu in bigMenu:
-  html = requests.get(menu)
-  soup = BeautifulSoup(html.text, "html.parser")
-  categories = soup.findAll('div', {"class" : "sidebar-navigation--item sidebar-navigation--category"})
-  for link in categories:
-    allLinks = link.findAll("a")
-    for a in allLinks:
-      totalLinks.append(a["href"])
+  driver = webdriver.PhantomJS()
+  driver.get(menu)
+  html = driver.page_source
+  soup = BeautifulSoup(html, "html.parser")
+  print "new page"
+  headers = soup.findAll("span", class_="sidebar-navigation--header--text")
+  for head in headers:
+    if head.text.strip() == "Shop by Category":
+      parent = head.findParent("h2")
+      newlist = parent.findAllNext("div", class_="sidebar-navigation--item sidebar-navigation--category")
+      for item in newlist:
+        # new addition
+        links = item.findAll("a")
+        for i in links:
+          linkTotal = linkTotal + 1
+          totalLinks.append(i["href"])
+        itemNext = item.findNextSibling()
+        nextHeadSpan = itemNext.find("span", class_="sidebar-navigation--header--text")
+        if nextHeadSpan is not None:
+          print "head span found"
+          nextText = nextHeadSpan.text.strip()
+          if nextText == "Deals":
+            print "oops: " + nextText
+            break
+      if nextText == "Deals":
+        break
 
+print "got links"
+print linkTotal
+
+#ERROR SPOT!
+#issues with accessing all of the products on the page
 for link in totalLinks:
-  new_url = base_url + link
-  print new_url
-  html = requests.get(new_url)
-  soup = BeautifulSoup(html.text, "html.parser")
-  while len(soup.select("div.product-photo a")) != 0:
-    for a in soup.select("div.product-photo a"):
+  print "link: " + link
+  driver = webdriver.PhantomJS()
+  driver.get(link)
+  html = driver.page_source
+  soup = BeautifulSoup(html, "html5lib")
+  bigDiv = soup.findAll("div", class_="sp_sm spacing_small")
+  for div in bigDiv:
+    links = div.findAll("a")
+    for i in links:
       j = j + 1
-      productUrl = base_url + a["href"]
+      productUrl = base_url + i["href"]
       print productUrl
-      # postProduct(productUrl)
+        # print productUrl
+        # postProduct(productUrl)
     print j
 
 print j
